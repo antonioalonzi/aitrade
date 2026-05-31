07828a4c5d74793ef1767648459ad9edbe4ca26f
Antonio Alonzi <antonioalonzi85@gmail.com>
2026-05-31 17:22:58 +0100

    Getting data from api

--- ig_client.py
+++ ig_client.py
@@ -1,8 +1,10 @@
 import os
 import logging
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from trading_ig import IGService
 from trading_ig.config import config
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("IGClient")
@@ -13,7 +15,7 @@ class IGTradingClient:
 
         self.acc_number = os.getenv("IG_SERVICE_ACC_NUMBER")
 
-        self.service = IGService(
+        self.ig_service = IGService(
             os.getenv("IG_SERVICE_USERNAME"),
             os.getenv("IG_SERVICE_PASSWORD"),
             os.getenv("IG_SERVICE_API_KEY"),
@@ -23,17 +25,36 @@ class IGTradingClient:
 
     def connect(self):
         try:
-            self.service.create_session()
-            self.service.switch_account(os.getenv("IG_SERVICE_ACC_NUMBER"), False)
+            self.ig_service.create_session()
+            self.ig_service.switch_account(os.getenv("IG_SERVICE_ACC_NUMBER"), False)
             logger.info("Successfully authenticated.")
 
         except Exception as e:
             logger.error(f"Authentication failed: {e}")
             raise
 
-    def get_market_info(self, epic: str):
+    def fetch_prices_last_14_days(self, epic: str):
+        return self._fetch_historical_prices_by_epic_and_date_range(epic, 'D', datetime.now() - timedelta(days=14), datetime.now())
+
+    def fetch_prices_last_3_days(self, epic: str):
+        return self._fetch_historical_prices_by_epic_and_date_range(epic, '1h', datetime.now() - timedelta(days=3), datetime.now())
+
+    def fetch_prices_last_12_hours(self, epic: str):
+        return self._fetch_historical_prices_by_epic_and_date_range(epic, '15MIN', datetime.now() - timedelta(hours=12), datetime.now())
+
+    def fetch_prices_last_1_hour(self, epic: str):
+        return self._fetch_historical_prices_by_epic_and_date_range(epic, '1MIN', datetime.now() - timedelta(hours=1), datetime.now())
+
+    def search_markets(self):
+        search_results = self.ig_service.search_markets("NVIDIA")
+        print(f"search_results: {search_results}")
+
+    def _fetch_historical_prices_by_epic_and_date_range(self, epic: str, resolution: str, start_date: datetime, end_date: datetime):
         try:
-            return self.service.fetch_market_by_epic(ep ic)
+            start_str = start_date.strftime("%Y-%m-%d %H:%M:%S")
+            end_str = end_date.strftime("%Y-%m-%d %H:%M:%S")
+            return self.ig_service.fetch_historical_prices_by_epic_and_date_range(epic, resolution, start_date, end_date)
         except Exception as e:
             logger.error(f"Failed to fetch market data for {epic}: {e}")
             return None
+
--- main.py
+++ main.py
@@ -12,14 +12,19 @@ logging.basicConfig(
 logger = logging.getLogger("Main")
 
 
+NVIDIA = "UC.D.NVDA.DAILY.IP"
+
+
 if __name__ == "__main__":
     try:
         client = IGTradingClient()
         client.connect()
-        ticker = "BTC" # continue from here
-        logger.info(f"Triggering workflow for {ticker}...")
-        instrument_id = client.get_instrument_id(ticker)
-        logger.info(f"Successfully received data from client! ID is: {instrument_id}")
+        epic = NVIDIA
+        prices_last_14_days = client.fetch_prices_last_14_days(epic)
+        prices_last_3_days = client.fetch_prices_last_3_days(epic)
+        prices_last_12_hours = client.fetch_prices_last_12_hours(epic)
+        prices_last_1_hour = client.fetch_prices_last_1_hour(epic)
+        logger.info(f"Successfully received data from client! ID is: {prices_last_14_days}\n {prices_last_3_days}\n {prices_last_12_hours}\n {prices_last_1_hour}")
 
     except Exception as e:
         print(f"API Integration Error: {e}")
