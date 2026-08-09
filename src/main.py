a92ecd97c5d1955abb862c35a71f8cbf615f3771
Antonio Alonzi <antonioalonzi85@gmail.com>
2026-08-09 16:28:19 +0200

    logs rotation

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+from logging.handlers import TimedRotatingFileHandler
 
 from ai_data_downloader import run_data_downloader
 from ai_trader import run_trader
@@ -10,7 +11,13 @@ logging.basicConfig(
     format="%(asctime)s [%(levelname)s] [%(name)s] %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
     handlers=[
-        logging.FileHandler("../logs/aitrade.log"),
+        TimedRotatingFileHandler(
+            filename="../logs/aitrade.log",
+            when="D",
+            interval=14,
+            backupCount=12,
+            encoding="utf-8"
+        ),
         logging.StreamHandler(sys.stdout)
     ]
 )
